915df89022eb34a4d0b481e5fed842a9166c1d5f
baffeto <baffeto@mail.ru>
2023-06-28 13:46:51 +0300

    gulp + maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass'));
-
+var autoprefixer = require('gulp-autoprefixer'); // npm install --save-dev gulp-autoprefixer
+var sourcemaps = require('gulp-sourcemaps');
 
 function copy(done) {
     gulp.src('./scss/style.scss')
+      .pipe(sourcemaps.init())
       .pipe( sass({
         errorLogToConsole: true,
         outputStyle: 'compressed'
       }) )
       .on('error', console.error.bind(console))
+      .pipe(autoprefixer({
+        overrideBrowserslist:  ['last 2 versions'],
+        cascade: false
+      }))
       .pipe( rename({suffix: '.min'}) )
+      .pipe(sourcemaps.write('./'))
       // .pipe( rename('main.css') ) // npm install --save-dev gulp-rename
       .pipe( gulp.dest('./css/') );
     //   .pipe();
